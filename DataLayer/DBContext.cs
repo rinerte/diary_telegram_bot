6140ad6d8b441e1be18b1d792de591cfe70449cf
rinerte <rinerte@gmail.com>
2024-09-01 20:52:04 +0900

    -

---
+++ DataLayer/DBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DataLayer.Models;
+
+namespace DataLayer
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options)
+        : base(options)
+        {
+           //Database.EnsureDeleted();
+           Database.EnsureCreated();
+        }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
---
+++ DataLayer/Services/DBService.cs
@@ -0,0 +1,22 @@
+﻿using DataLayer.Interfaces;
+
+namespace DataLayer.Services
+{
+    public partial class DBService : IDBService
+    {
+        DBContext _context;
+        public DBService(DBContext context)
+        {
+            _context = context;
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                _context.Dispose();
+            }
+            catch { }
+        }
+    }
+}
